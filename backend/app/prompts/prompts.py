d2a01ac19789df9263f98cb066833d70ae313978
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2025-08-17 11:11:35 +0530

    providing a better system prompt to the analyzer

--- backend/app/prompts/prompts.py
+++ backend/app/prompts/prompts.py
@@ -1,4 +1,7 @@
 import os
+import pandas as pd
+from io import StringIO
+from textwrap import dedent
 
 prompt_type_to_file_name_map = {
     "simple_chat": "simple_chat_system_prompt.txt",
@@ -56,3 +59,38 @@ class Prompts:
     SIMPLE_CHAT = load_prompt("simple_chat")
     SIMPLE_CHAT_WITH_IMAGE = load_prompt("simple_chat_with_image")
     DATA_ANALYZER = load_prompt("data_analyzer")
+
+    @staticmethod
+    def format_system_prompt_for_analyzer(df: pd.DataFrame) -> str:
+        """
+        Format the prompt for data analysis with the given DataFrame.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            The DataFrame to analyze.
+
+        Returns
+        -------
+        str
+            The formatted prompt for data analysis.
+        """
+        info = "\n\n## Dataframe Information\nYou have access to a pandas DataFrame named `df`. It contains the dataset you need to analyze. The DataFrame has the following columns:\n\n"
+        columns = df.columns.tolist()
+        columns = ", ".join(columns)
+        info += f"{columns}\n\n"
+
+        buffer = StringIO()
+        df.info(buf=buffer)
+        info_ = buffer.getvalue()
+
+        info += (
+            f"A brief description of the DataFrame is provided below:\n\n{info_}\n\n"
+        )
+
+        head_info = df.head(min(5, len(df))).to_markdown(index=False, tablefmt="json")
+        info += f"The first 5 rows of the DataFrame are as follows:\n\n{head_info}\n\n"
+
+        info = dedent(info).strip()
+        final_prompt = Prompts.DATA_ANALYZER + info
+        return final_prompt
--- backend/app/services/llm.py
+++ backend/app/services/llm.py
@@ -13,13 +13,11 @@ from ..models import AnalysisResponseModalChatbot, AnalyzeResponse
 from ..utils import create_simple_logger
 
 load_dotenv()
-# import litellm
-
-# litellm._turn_on_debug()
 
 model = os.getenv("LLM_MODEL", "gemini/gemini-2.0-flash")
 session_storage = SessionStorage()
-logger = create_simple_logger("llm_service")
+logger = create_simple_logger(__name__)
+MAX_MESSAGES = 20
 
 
 async def atext_completion(messages: List[Dict[str, Any]], **kwargs: Dict) -> str:
@@ -148,9 +146,9 @@ async def vision_completion(message: str, image_bytes: bytes, session_id: str) -
         current_message,
     ]
 
-    if len(messages) > 10:
-        # Limit to last 10 messages for performance
-        messages = messages[-10:]
+    if len(messages) > MAX_MESSAGES:
+        # Limit to last MAX_MESSAGES messages for performance
+        messages = messages[-MAX_MESSAGES:]
 
     try:
         response = await atext_completion(messages, session_id=session_id)
@@ -163,16 +161,6 @@ async def vision_completion(message: str, image_bytes: bytes, session_id: str) -
     return response
 
 
-def get_info_about_df(df: pd.DataFrame) -> str:
-    columns = df.columns.tolist()
-    columns_str = ", ".join(columns)
-    info = f"The dataframe that you have acces to has the following columns: {columns_str}."
-    buffer = StringIO()
-    df.info(buf=buffer)
-    info += f"\n\nDataFrame info:\n{buffer.getvalue()}"
-    return info
-
-
 async def analyze_data(
     df: pd.DataFrame,
     message: str,
@@ -183,10 +171,13 @@ async def analyze_data(
     """
     past_messages = session_storage.get_messages(session_id)
     if past_messages is None:
-        past_messages = [{"role": "system", "content": Prompts.DATA_ANALYZER}]
+        past_messages = [
+            {
+                "role": "system",
+                "content": Prompts.format_system_prompt_for_analyzer(df),
+            }
+        ]
 
-    df_info = get_info_about_df(df)
-    message = f"{df_info}\n\n{message}"
     current_message = {"role": "user", "content": message}
     session_storage.push_messages(session_id, current_message)
     messages = [
@@ -194,25 +185,24 @@ async def analyze_data(
         current_message,
     ]
 
-    if len(messages) > 10:
-        # Limit to last 10 messages for performance
-        messages = messages[-10:]
+    if len(messages) > MAX_MESSAGES:
+        # Limit to last MAX_MESSAGES messages for performance
+        messages = messages[-MAX_MESSAGES:]
 
     try:
         response = await atext_completion(
             messages,
             session_id=session_id,
             response_format=AnalysisResponseModalChatbot,
-            max_tokens=1000,
             temperature=0.5,
         )
         session_storage.push_messages(
             session_id, {"role": "assistant", "content": response}
         )
     except Exception as e:
-        logger.error(f"Error during data analysis: {e}")
+        logger.error(f"Error during calling the LLM for data analysis: {e}")
         return AnalyzeResponse(
-            reply="Error during data analysis. Please try again.",
+            reply="Error during calling the LLM for data analysis. Please try again.",
             code="",
             artifact="",
             artifact_is_mime_type=False,
